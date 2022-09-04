f8c10159d3d5a7b2c5c535f56529cec4ceee3118
tientruongcao51 <32028093+tientruongcao51@users.noreply.github.com>
2022-09-04 11:07:19 +0700

    Add files via upload

---
+++ function/notification/__init__.py
@@ -0,0 +1,41 @@
+import logging
+import azure.functions as func
+import psycopg2
+import os
+from datetime import datetime
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
+
+
+def main(msg: func.ServiceBusMessage):
+    notification_id = int(msg.get_body().decode('utf-8'))
+    logging.info('Python ServiceBus queue trigger processed message: %s', notification_id)
+
+    conn = psycopg2.connect("dbname=test user=postgres")
+    try:
+        cur = conn.cursor()
+        cur.execute("SELECT message,subject FROM notification WHERE id=%s;", (notification_id,))
+        messagePlain, subject = cur.fetchone()
+        cur.execute("SELECT email,first_name FROM attendee;")
+        attendees = cur.fetchall()
+        for attendee in attendees:
+            message = Mail(
+                from_email='from_email@example.com',
+                to_emails=attendee[0],
+                subject='{}: {}'.format(attendee[1], subject),
+                html_content=messagePlain)
+            try:
+                sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
+                response = sg.send(message)
+                print(response.status_code)
+                print(response.body)
+                print(response.headers)
+            except Exception as e:
+                print(str(e))
+        total_attendees = 'Notified {} attendees'.format(len(attendees))
+        cur.execute("UPDATE notification SET status = %s WHERE id=%s;", (total_attendees, notification_id))
+        conn.commit()
+    except (Exception, psycopg2.DatabaseError) as error:
+        logging.error(error)
+    finally:
+        conn.close()
---
+++ web/config.py
@@ -0,0 +1,28 @@
+import os
+
+app_dir = os.path.abspath(os.path.dirname(__file__))
+
+
+class BaseConfig:
+    DEBUG = True
+    POSTGRES_URL = os.getenv('POSTGRES_URL')
+    POSTGRES_USER = os.getenv('POSTGRES_USER')
+    POSTGRES_PW = os.getenv('POSTGRES_PW')
+    POSTGRES_DB = os.getenv('POSTGRES_DB')
+    DB_URL = 'postgresql://{user}:{pw}@{url}/{db}'.format(user=POSTGRES_USER, pw=POSTGRES_PW, url=POSTGRES_URL,
+                                                          db=POSTGRES_DB)
+    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI') or DB_URL
+    CONFERENCE_ID = 1
+    SECRET_KEY = os.getenv('SECRET_KEY', 'LWd2tzlprdGHCIPHTd4tp5SBFgDszm')
+    SERVICE_BUS_CONNECTION_STRING = os.getenv('SERVICE_BUS_CONNECTION_STRING')
+    SERVICE_BUS_QUEUE_NAME = os.getenv('SERVICE_BUS_QUEUE_NAME')
+    ADMIN_EMAIL_ADDRESS: 'info@techconf.com'
+    SENDGRID_API_KEY = ''  # Configuration not required, required SendGrid Account
+
+
+class DevelopmentConfig(BaseConfig):
+    DEBUG = True
+
+
+class ProductionConfig(BaseConfig):
+    DEBUG = False
