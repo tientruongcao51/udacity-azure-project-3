cc55bf1c6a86ef586efa3b477f0d9265f055925e
truongct <tientruongcao51@gmail.com>
2022-09-04 13:40:55 +0700

    update connect DB

--- function/notification/__init__.py
+++ function/notification/__init__.py
@@ -11,11 +11,12 @@ def main(msg: func.ServiceBusMessage):
     notification_id = int(msg.get_body().decode('utf-8'))
     logging.info('Python ServiceBus queue trigger processed message: %s', notification_id)
 
-    conn = psycopg2.connect("dbname=test user=postgres")
+    conn = conn = psycopg2.connect(host=os.environ.get('DB_HOST'), database=os.environ.get('DB_DATABASE'),
+                                   user=os.environ.get('DB_USER'), password=os.environ.get('DB_PASSWORD'))
     try:
         cur = conn.cursor()
         cur.execute("SELECT message,subject FROM notification WHERE id=%s;", (notification_id,))
-        message_plain, subject = cur.fetchone()
+        messagePlain, subject = cur.fetchone()
         cur.execute("SELECT email,first_name FROM attendee;")
         attendees = cur.fetchall()
         for attendee in attendees:
@@ -23,7 +24,7 @@ def main(msg: func.ServiceBusMessage):
                 from_email='from_email@example.com',
                 to_emails=attendee[0],
                 subject='{}: {}'.format(attendee[1], subject),
-                html_content=message_plain)
+                html_content=messagePlain)
             try:
                 sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
                 response = sg.send(message)
@@ -32,6 +33,7 @@ def main(msg: func.ServiceBusMessage):
                 print(response.headers)
             except Exception as e:
                 print(str(e))
+        # Update the notification table by setting the completed date and updating the status with the total number of attendees notified
         total_attendees = 'Notified {} attendees'.format(len(attendees))
         cur.execute("UPDATE notification SET status = %s WHERE id=%s;", (total_attendees, notification_id))
         conn.commit()
